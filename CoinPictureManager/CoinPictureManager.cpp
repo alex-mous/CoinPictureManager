97a29deb3e73742d37d9503fb5e279b3d78ed955
Alex Mous <48971842+polarpiberry@users.noreply.github.com>
2020-08-24 22:40:41 -0700

    Add LICENSE

--- CoinPictureManager/CoinPictureManager.cpp
+++ CoinPictureManager/CoinPictureManager.cpp
@@ -1,11 +1,31 @@
-// Chroma Key.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/*
+ * CoinPictureManager.cpp - Handles main interaction with the user (creates and runs a console window)
+ * This file is part of CoinPictureManager.
+ *
+ * Copyright (C) 2020  PolarPiBerry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <iostream>
-#include "ChromaKey.h"
-#include "Crop.h"
 #include <filesystem>
 #include <string>
+
+#include "ChromaKey.h"
+#include "Crop.h"
+
+
 
 using namespace std;
 
--- CoinPictureManager/Crop.cpp
+++ CoinPictureManager/Crop.cpp
@@ -1,5 +1,22 @@
-// Crop.cpp : This file contains the cropping functions
-//
+/*
+ * Crop.cpp - Find the bounding box of the coin in the image and crop the image to it (user selection window for padding around coin)
+ * This file is part of CoinPictureManager.
+ *
+ * Copyright (C) 2020  PolarPiBerry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <iostream>
 #include <chrono>
